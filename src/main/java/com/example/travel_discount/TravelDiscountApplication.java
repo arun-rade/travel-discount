2d4ca4ea9a3e87a296bd93b3bea6b32be3a6b8ea
arun.rade <arun.rade@gmail.com>
2025-01-20 15:39:52 +0530

    java code travel discount

---
+++ src/main/java/com/example/travel_discount/TravelDiscountApplication.java
@@ -0,0 +1,13 @@
+package com.example.travel_discount;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelDiscountApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravelDiscountApplication.class, args);
+	}
+
+}
